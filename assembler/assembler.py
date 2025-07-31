a9a7ccf21d0b05bcd03cbd629f5ef9382c211bee
Karlo Godfrey Escalona Gregorio <kgregorio9@yahoo.com>
2025-07-30 20:29:34 -0700

    Updated ISA specs, minor progress to assembler

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,12 +1,47 @@
 import sys 
 import csv
+import re
+
 
 if len(sys.argv) < 2:
     print("Usage: python assembler.py <filename.s>")
     sys.exit(1)
 
-filename = sys.argv[1]
+assembly_file = sys.argv[1]
 
-with open(filename, 'r') as file:
+mneumonics = {
+    "add": "0000",
+    "sub": "0001"
+}
+
+symbols = {
+
+}
+
+xorf = {
+    "x": "11",
+    "f": "10",
+    "d": "01",
+    "b": "00"
+}
+
+cond = {
+    "al": "0001" 
+}
+
+# instead add "s" at the end on whether or not to alter code and parse the pnemonic. e.g. When getting to the mneumonic, look through the pneumonic until an "s" is reached
+# alter_code = {
+#     "s": "1",
+#     "n": "0"
+# }
+
+lc = 0
+with open(assembly_file, 'r') as file:
     for line in file:
-        print(line.strip())
+        # empty line
+        if line.strip() == "":
+            continue 
+        else: 
+            # Split by commas and whitespace
+            tokens = re.findall(r'\w+|[^\s\w]', line)
+            print("Tokens:", tokens)
--- assembler/test_assembly_01.s
+++ assembler/test_assembly_01.s
@@ -1,1 +1,19 @@
-addi.x r0, #9
+main: 
+
+add.x-al r1, r2, r3
+
+addi.x-al r1, #9
+
+sub.x-al r1, r2, r3
+
+subi.x-al r1, #9
+
+adds.x-al r1, r2, r3
+
+addis.x-al r1, #9
+
+procedure: 
+
+subs.x-al r1, r2, r3
+
+subis.x-al r1, #9
