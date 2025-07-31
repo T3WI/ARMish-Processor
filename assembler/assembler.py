97ca39ae33c720df2a1666da3099e350e89047f8
Karlo Godfrey Escalona Gregorio <kgregorio9@yahoo.com>
2025-07-30 21:59:22 -0700

    Completed assembler's first pass, and updated documentation

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -35,6 +35,10 @@ cond = {
 #     "n": "0"
 # }
 
+### FIRST PASS
+print('-' * 50)
+print("FIRST PASS")
+print('-' * 50)
 lc = 0
 with open(assembly_file, 'r') as file:
     for line in file:
@@ -43,5 +47,49 @@ with open(assembly_file, 'r') as file:
             continue 
         else: 
             # Split by commas and whitespace
-            tokens = re.findall(r'\w+|[^\s\w]', line)
-            print("Tokens:", tokens)
+            token = re.findall(r'\w+|[^\s\w]', line)
+            print(lc, ":", token)
+            # Identify labels and store in symbol table
+            if token[1] == ':':
+                symbols[token[0]] = lc
+                continue
+            lc = lc + 4
+print(symbols)
+print('-' * 50)
+print("FIRST PASS COMPLETE")
+print('-' * 50)
+
+# right now just updates with pc and the instruction, need to remove line and output binary form of instruction
+def second_pass(token, lc, line):
+    return str(lc) + ":" + line 
+
+### SECOND PASS
+print('-' * 50)
+print("SECOND PASS")
+print('-' * 50)
+lc = 0
+with open("out.bin", "w") as f:
+    with open(assembly_file, 'r') as file:
+        for line in file:
+            # empty line
+            if line.strip() == "":
+                continue 
+            else: 
+                # Split by commas and whitespace
+                token = re.findall(r'\w+|[^\s\w]', line)
+                if token[1] == ':':                         # label case
+                    continue
+            
+
+                ###
+                # TODO: SECOND PASS
+                instr_bin = second_pass(token, lc, line)
+                ###     
+
+
+                f.write(instr_bin)
+                lc = lc + 4
+
+print('-' * 50)
+print("SECOND PASS COMPLETE")
+print('-' * 50)
