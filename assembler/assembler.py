4dad5bbce05d26f3b617f18c128dcce8a39c8c00
Karlo Godfrey Escalona Gregorio <kgregorio9@yahoo.com>
2025-08-05 16:14:53 -0700

    Assembler Update
    Assembler can parse the cond field and opcode

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,9 +1,17 @@
 import sys 
 import csv
 import re
+from enum import Enum
 
-FORMAT_DEBUG = 1
-FORMAT_PROD = 0
+class Formatter(Enum):
+    PROD = 0
+    DEBUG = 1
+
+class Instruction_Type(Enum):
+    R = 0
+    OP3 = 1
+    D = 2
+    B = 3
 
 if len(sys.argv) < 2:
     print("Usage: python assembler.py <filename.s>")
@@ -12,23 +20,66 @@ if len(sys.argv) < 2:
 assembly_file = sys.argv[1]
 
 mneumonics = {
-    "add": "0000",
-    "sub": "0001"
+    "addx": "110000",
+    "subx": "110001",
+    "mulx": "110010",
+    "divx": "110011",
+    "notx": "110100",
+    "andx": "110101",
+    "orrx": "110110",
+    "convx": "110111",
+    "addf": "111000",
+    "subf": "111001",
+    "mulf": "111010",
+    "divf": "111011",
+    "cmpf": "111100",
+    "cnvf": "111101",
+    "sqrf": "111110",
+    "recf": "111111",
+
+    "vmac": "100000",
+    "vadd": "100001",
+    "vsub": "100010",
+    "vsel": "100011",
+
+    "ldw": "010000",
+    "ldb2l": "010001",
+    "ldb2h": "010010",
+    "stw": "010011",
+    "stb2l": "010100",
+    "stb2h": "010101",
+
+    "bx": "001000",
+    "b": "000000",
+    "bl": "000100"
+
+    
+
+
+
+
 }
 
 symbols = {
 
 }
 
-xorf = {
-    "x": "11",
-    "f": "10",
-    "d": "01",
-    "b": "00"
-}
-
 cond = {
-    "al": "0001" 
+    "al": "0001",
+    "le": "0010",
+    "gt": "0011",
+    "lt": "0100",
+    "ge": "0101",
+    "ls": "0110",
+    "hi": "0111",
+    "vc": "1000",
+    "vs": "1001",
+    "pl": "1010",
+    "mi": "1011",
+    "cc": "1100",
+    "cs": "1101",
+    "neq": "1110",
+    "eq": "1111",
 }
 
 # instead add "s" at the end on whether or not to alter code and parse the pnemonic. e.g. When getting to the mneumonic, look through the pneumonic until an "s" is reached
@@ -62,18 +113,101 @@ print("FIRST PASS COMPLETE")
 print('-' * 50)
 
 # right now just updates with pc and the instruction
-def format_machine_code(machine_code, lc, line, format=FORMAT_PROD):
-    if format == 1:
-        formatted_machine_code = str(lc) + "\t:" + "\t\t" + machine_code + "\t(" + line + ")\n" 
+def format_machine_code(machine_code, lc, line, output_mode=Formatter.PROD):
+    if output_mode == Formatter.DEBUG:
+        binary_lc = format(lc, '08b')
+        hex_lc = format(lc, '08x')
+        return f"{lc:>3} ({binary_lc}) ({hex_lc}):\t{machine_code}\t({line})\n"
     else:
-        formatted_machine_code = str(lc) + "\t:" + "\t\t" + machine_code + "\n" 
-    return formatted_machine_code
+        binary_lc = format(lc, '08b')
+        return f"{binary_lc} :\t{machine_code}\n"
+    
+def set_instr_type(mneumonic_code):
+    if mneumonic_code[:2] == "11":
+        return Instruction_Type.R
+    elif mneumonic_code[:2] == "10":
+        return Instruction_Type.OP3
+    elif mneumonic_code[:2] == "01":
+        return Instruction_Type.D 
+    elif mneumonic_code[:2] == "00":
+        return Instruction_Type.B 
+    else:
+        raise Exception("Invalid Type")
+    
+# TODO: PARSE BITS 21:0
+def parse_r(token):
+    set_cpsr = 0
+    cond_bits = ""
+    r_d = ""
+    operation_bits = mneumonics[token[0]]
 
-def second_pass(token, lc, line):
-    machine_code = "0101_11_1_0_1010_0111_0001_0_001_10101010" # TODO
+
+    if token[1] == '.':
+        if token[2] == 's':
+            set_cpsr = 1
+        # if I need more . flags, add more
+    if token[1] == '-':
+        cond_bits = cond[token[2]]
+
+
+    if token[3] == '-':
+        cond_bits = cond[token[4]]
+    if token[3][:1] == 'r':
+        r_d = token[3][:-1]
 
     
-    machine_code_f = format_machine_code(machine_code, lc, line, FORMAT_DEBUG)                      # debug line
+    machine_code = cond_bits + operation_bits
+    return machine_code
+
+# TODO: PARSE BITS 21:0
+def parse_op3(token):
+    set_cpsr = 0
+    operation_bits = token[0]
+
+
+    machine_code = operation_bits
+    return machine_code
+
+# TODO: PARSE BITS 31:0
+def parse_d(token):
+    operation_bits = mneumonics[token[0]]
+    cond_bits = cond[token[2]]
+
+    machine_code = cond_bits + operation_bits
+    return machine_code
+
+# TODO: PARSE BITS 31:0
+def parse_b(token):
+    operation_bits = mneumonics[token[0]]
+    cond_bits = cond[token[2]]
+
+    machine_code = cond_bits + operation_bits
+    return machine_code
+
+
+def second_pass(token, lc, line):
+    dot_flag = 0
+    dash_flag = 0
+    set_cpsr_true = 0
+    cond_field = ""
+    instr_type = ""
+    r_n = ""
+    r_d = ""
+
+    mneumonic_code = mneumonics[token[0]]
+    instr_type = set_instr_type(mneumonic_code)
+    if instr_type == Instruction_Type.R:
+        machine_code = parse_r(token)
+    elif instr_type == Instruction_Type.OP3:
+        machine_code =  parse_op3(token)
+    elif instr_type == Instruction_Type.D:
+        machine_code =  parse_d(token)
+    elif instr_type == Instruction_Type.B:
+        machine_code =  parse_b(token)
+
+
+    # CHANGE THE 4TH ARGUMENT TO CHANGE THE OUTPUT FORMAT OF THE BIN
+    machine_code_f = format_machine_code(machine_code, lc, line, Formatter.DEBUG)                      # debug line
     return machine_code_f
 
 ### SECOND PASS
@@ -96,7 +230,7 @@ with open("out.bin", "w") as f:
 
                 ###
                 # TODO: SECOND PASS
-                instr_bin = second_pass(token, lc, line[:-1])
+                instr_bin = second_pass(token, lc, line.strip())
                 ###     
 
 
--- assembler/test_assembly_01.s
+++ assembler/test_assembly_01.s
@@ -1,85 +1,73 @@
 regx: 
 
-add.x-al r1, r2, r3
+addx-al r1, r2, r3
 
-add.x-al r1, #9
+addx-al r1, #9
 
-add.x-al r1, r2, r3, lsl #2
+addx-al r1, r2, r3, lsl #2
 
-add.x-al r1, #9, lsl #2
+addx-al r1, #9, lsl #2
 
-sub.x-al r1, r2, r3
+subx-al r1, r2, r3
 
-sub.x-al r1, #9
+subx-al r1, #9
 
-sub.x-al r1, r2, r3, lsl #2
+subx-al r1, r2, r3, lsl #2
 
-sub.x-al r1, #9, lsl #2
+subx-al r1, #9, lsl #2
 
-adds.x-al r1, r2, r3
+addx.s-al r1, r2, r3
 
-adds.x-al r1, #9
+addx.s-al r1, #9
 
-subs.x-al r1, r2, r3
+subx.s-al r1, r2, r3
 
-subs.x-al r1, #9
+subx.s-al r1, #9
 
-mul.x-eq r1, r2, r3
+mulx-eq r1, r2, r3
 
-mul.x-eq r1, r2, #9
+mulx-eq r1, r2, #9
 
-div.x-eq r1, r2, r3
+divx-eq r1, r2, r3
 
-div.x-eq r1, r2, #9
+divx-eq r1, r2, #9
 
-mac.x-eq r1, r2, r3
+notx-eq r2, r1
 
-sqrt.x-eq r1, r2
+andx-al r3, r1, r2
 
-convf.x-eq r1, r2
+andx-al r2, r1, #9
 
-cmp.x-eq r1, r2
+orrx-al r3, r1, r2
 
-regf:
-
-add.f-al r1, r2, r3, #0
-
-sub.f-al r1, r2, r3, #0
-
-mul.f-eq r1, r2, r3, #0
-
-div.f-eq r1, r2, r3, #0
-
-mac.f-eq r1, r2, r3, #0
-
-sqrt.f-eq r1, r2, #0
-
-convx.f-eq r1, r2, #0
-
-cmp.f-eq r1, r2
+orrx-al r2, r1, #9
 
 regd:
 
-ldr r1, [r2, #8]
+ldw-al r2, [r1, #2]
 
-str r1, [r2, #8]
+ldw-al r2, [r1, #-2]
 
-ldr r1, [r2, #-8]
+ldw-al r2, [r1, r3]
 
-str r1, [r2, #-8]
+ldb2l-al r2, [r1, #0]
 
-ldr r1, [r2, r3]
+stw-al r2, [r1, #2]
 
-str r1, [r2, r3]
+stw-al r2, [r1, #-2]
 
-ldrb r1, [r2, #8]
+stw-al r2, [r1, r3]
 
-strb r1, [r2, #8]
+stb2l-al r2, [r1, #0]
 
 regb:
 
-bx r1 
+bx-al lr
 
-b regd
+b-al label
 
-bl regf
+subx.s-al r0, r1, r2
+
+b-eq label
+
+bl-al label
