b11a92db8f0df169d03be57d3e22f262feeacd08
Karlo Godfrey Escalona Gregorio <kgregorio9@yahoo.com>
2025-08-09 14:23:14 -0700

    Top view of processor, PC adder

---
+++ processor/armish_processor/armish_processor.srcs/sources_1/new/top.sv
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/09/2025 01:28:35 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    output logic [15:0] pc_next, 
+    input clk,              // clock 
+    input reset             // synchronous reset
+    );
+    logic [15:0] pc;
+    logic [15:0] offset;
+    
+    assign offset = 16'd4;
+    pc_adder no_branch_adder(.pc_next(pc_next), .pc(pc), .offset(offset));
+    always_ff@(posedge clk) begin
+        if(reset) begin 
+            pc <= 16'd0;
+        end
+        else begin 
+            pc <= pc_next;
+        end
+    end
+    assign sig = 1'b1;
+    
+endmodule
+
+
+
+
+
+
+
+
+
+
--- processor/armish_processor/armish_processor.srcs/top_sim/new/top_sim.sv
+++ processor/armish_processor/armish_processor.srcs/top_sim/new/top_sim.sv
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 08/09/2025 12:36:30 PM
+// Create Date: 08/09/2025 02:05:50 PM
 // Design Name: 
-// Module Name: pc_adder
+// Module Name: top_sim
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,11 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module pc_adder(
-    output [15:0] pc_next,
-    input [15:0] pc,
-    input [15:0] offset
-    );
-    assign pc_next = pc + offset;
-endmodule
+module top_sim(
 
+    );
+    logic [15:0] out;
+    logic reset;
+    
+    reg clk;
+    
+    initial begin
+        clk = 1'b0;
+        forever #5 clk = ~clk;
+    end
+    
+    top t(.pc_next(out), .clk(clk), .reset(reset));
+    
+    initial begin
+        reset = 1'b1;
+        #10;
+        reset = 1'b0;
+    end 
+    
+endmodule
